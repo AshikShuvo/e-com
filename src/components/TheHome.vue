160885def8b0c8e6c1f83b3729cd64dfe4ebc745
AshikShuvo <ashikshuvo1996@gmail.com>
2021-12-06 12:48:41 +0600

    header shape

---
+++ src/components/TheHome.vue
@@ -0,0 +1,20 @@
+<template>
+   
+        <header class="w-full h-20">
+            <NavBar />
+        </header>
+        <main></main>
+        <footer></footer>
+  
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import NavBar from './NavBar.vue'
+
+export default defineComponent({
+  components: { NavBar },
+    setup() {
+        
+    },
+})
+</script>
